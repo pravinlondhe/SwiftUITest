ce50fcb4221b768812bf3546fa75b18424ec2360
Pravin Londhe <pravinlondhe15@gmail.com>
2021-06-07 06:51:29 +0530

    Initial Commit

---
+++ SwiftUITest/SwiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestApp.swift
+//  SwiftUITest
+//
+//  Created by Pravin Londhe on 07/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
