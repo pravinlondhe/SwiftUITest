4d1e85494ad3bba798e2748f10a8d4d8f764fcb7
Pravin Londhe <pravinlondhe15@gmail.com>
2021-06-08 17:34:19 +0530

    lesson-5 war game UI

---
+++ SwiftUITest/Lesson5.swift
@@ -0,0 +1,65 @@
+//
+//  Lesson5.swift
+//  SwiftUITest
+//
+//  Created by Pravin Londhe on 08/06/21.
+//
+
+import SwiftUI
+
+struct Lesson5Preview: PreviewProvider{
+    static var previews: some View{
+        Lesson5Practise()
+    }
+}
+
+struct Lesson5Practise:View {
+    var body: some View{
+        ZStack{
+            Image("background")
+                .ignoresSafeArea()
+        VStack{
+            Spacer()
+          Image("logo-1")
+            Spacer()
+            HStack{
+                Spacer()
+                Image("card2")
+                Spacer()
+                Image("card3")
+                Spacer()
+            }
+            Spacer()
+            Image("dealbutton").padding()
+            Spacer()
+            
+            HStack{
+                Spacer()
+                ScoreCard("Player","0")
+                Spacer()
+                ScoreCard("CPU", "0")
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
+}
+
+struct ScoreCard:View{
+    var name:String
+    var score: String
+    init(_ player:String , _ score:String ){
+        self.name = player
+        self.score = score
+    }
+    var body: some View{
+        VStack{
+            Text(name)
+            Text(score)
+                .padding()
+        }
+        .foregroundColor(.white)
+        .font(.largeTitle)
+    }
+}
